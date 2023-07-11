45e6c0aabd0901af2b62dfaab7316d5cb12c919d
dpkg123 <130299352+dpkg123@users.noreply.github.com>
2023-07-11 22:27:02 +0800

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,7 +9,7 @@ BUILD_CROSS_COMPILE=../aarch64-linux-android-4.9/bin/aarch64-linux-android-
 CLANG_PATH=../clang/bin
 CROSS_COMPILE_ARM32=../arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 
-CLANG_TRIPLE=aarch64-linux-gnu-
+CLANG_TRIPLE=../aarch64-linux-android-4.9/bin/aarch64-linux-android-
 
 export ARCH=arm64
 export PATH=${CLANG_PATH}:${PATH}
